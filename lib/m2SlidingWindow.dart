20d9fb59868f9713d7650f3fb0d6568888c4138d
Pankajkcodes <74593517+pankajkcodes@users.noreply.github.com>
2026-05-09 18:12:44 +0530

    Add files via upload

---
+++ lib/m2SlidingWindow.dart
@@ -0,0 +1,60 @@
+/*
+SLIDING WINDOW
+|
+|-- WHY?
+|     |
+|     |-- Faster
+|     |-- Avoid repeated work
+|     |-- Optimize brute force
+|
+|-- WHERE?
+|     |
+|     |-- Array
+|     |-- String
+|     |-- Subarray
+|     |-- Substring
+|     |-- Continuous elements
+|
+|-- TYPES
+|     |
+|     |-- Fixed Window
+|     |      |
+|     |      |-- Size fixed
+|     |      |-- Example:
+|     |             Max sum of size K
+|     |
+|     |-- Variable Window
+|            |
+|            |-- Size changes
+|            |-- Example:
+|                   Longest substring
+|
+|-- HOW?
+|     |
+|     |-- Remove left
+|     |-- Add right
+|     |-- Move window
+|
+|-- CONSTRAINTS
+|     |
+|     |-- Continuous range required
+|     |-- Sequential traversal
+|     |-- Reusable previous result
+|
+|-- TIME COMPLEXITY
+|     |
+|     |-- Brute Force = O(n*k)
+|     |-- Sliding Window = O(n)
+|
+|-- INTERVIEW KEYWORDS
+      |
+      |-- Longest
+      |-- Smallest
+      |-- Continuous
+      |-- Subarray
+      |-- Substring
+      |-- K size
+
+*/
+
+void main() {}
