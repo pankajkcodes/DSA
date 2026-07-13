602b946f34e12e282529fb1e3655deee1332a1f2
pankajkcodes-ai <pankaj.itofficework@gmail.com>
2026-07-13 12:22:09 +0530

    Add implementation to find the intersection of two integer arrays

---
+++ lib/03Masterji/q3.dart
@@ -0,0 +1,40 @@
+/*
+     Common Toys in Two Boxes
+
+     Question: Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in
+     the result must be unique and you may return the result in any order.
+
+     Example 1: Input: nums1 = [1,2,2,1], nums2 = [2,2] Output: [2]
+
+     Example 2: Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4] Output: [9,4]
+     Explanation: [4,9] is also accepted.
+
+     Constraints
+     1 <= nums1.length, nums2.length <= 1000
+     0 <= nums1[i], nums2[i] <= 1000
+
+ */
+
+void main() {
+  List<int> findCommon(List<int> nums1, List<int> nums2) {
+    Set<int> ans = {};
+
+    for (int i = 0; i < nums1.length; i++) {
+      for (int j = 0; j < nums2.length; j++) {
+        if (nums1[i] == nums2[j]) {
+          ans.add(nums2[j]);
+        }
+      }
+    }
+
+    return ans.toList();
+  }
+
+  // List<int> nums1 = [4,9,5];
+  // List<int> nums2 = [9,4,9,8,4];
+
+  List<int> nums1 = [1, 2, 2, 1];
+  List<int> nums2 = [2, 2];
+
+  print("Output-> ${findCommon(nums1, nums2)}");
+}
